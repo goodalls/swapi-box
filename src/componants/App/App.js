eda4071116d3656fed1740ecf4c7af4b7653ae96
Jeff Goodall <goodallhj@gmail.com>
2018-01-29 12:14:11 -0700

    updated the folder organization

--- src/componants/App/App.js
+++ src/componants/App/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
       <div className="App">
