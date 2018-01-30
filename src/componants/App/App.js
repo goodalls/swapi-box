fed895f2e65449384d204e3d68312d73479c8458
Jeff Goodall <goodallhj@gmail.com>
2018-01-29 20:52:14 -0700

    Updated the componants and App fetch

--- src/componants/App/App.js
+++ src/componants/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import Container from '../Container/Container';
 import Scrolling from '../Scrolling/Scrolling';
+import Control from '../Control/Control';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -9,17 +13,15 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://swapi.co/api/people/1')
-      .then(response => response.json())
+    this.fetchSwapi('https://swapi.co/api/people/1')
       .then(people => {
-        this.fetchSwapi(people.species[0])
-          .then(species => {
-            console.log(species);
-            console.log(people);
-            //setState
-          })
-          .catch(error => console.log('bummer'));
-      });
+        this.fetchSwapi(people.species[0]).then(species => {
+          console.log(species);
+          console.log(people);//scope
+          //setState
+        });
+      })
+      .catch(error => console.log('bummer'));
   }
 
   fetchSwapi(url) {
@@ -27,7 +29,15 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App"><Scrolling /></div>;
+    return (
+      <div className="App">
+        <Header />
+        <Control />
+        <Scrolling />
+        <Container />
+        <Footer />
+      </div>
+    );
   }
 }
 
--- src/componants/Control/Control.js
+++ src/componants/Control/Control.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 
 export class Control extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   
     this.state = {
        
-    }
+    };
   }
   
   render() {
     return (
       <div>
-        
+        <button>People</button>
+        <button>Planets</button>
+        <button>Vehicles</button>
+        <button>Favorites</button>
       </div>
-    )
+    );
   }
 }
 
-export default Control
+export default Control;
