0b751f6fff1c17b35488dd18a27c1ae26894f7fa
Jeff Goodall <goodallhj@gmail.com>
2018-01-31 13:23:45 -0700

    added api prop info into crawl

--- src/componants/Scrolling/Scrolling.js
+++ src/componants/Scrolling/Scrolling.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import './Scrolling.css';
+import PropTypes from 'prop-types';
 
-const Scrolling = () => {
+const Scrolling = ({ title, episodeId, openingCrawl, releaseDate }) => {
   return (
-    <div className='scrolling'>
+    <div className="scrolling">
       <div className="fade" />
 
       <section className="star-wars">
         <div className="crawl">
           <div className="title">
-            <p>Episode IV</p>
-            <h1>A New Hope</h1>
+            <p>Episode {episodeId}</p>
+            <h1>{title}</h1>
           </div>
+          <p>{openingCrawl}</p>
 
-          <p>
-            It is a period of civil war. Rebel spaceships, striking from a
-            hidden base, have won their first victory against the evil Galactic
-            Empire.
-          </p>
-          <p>
-            During the battle, Rebel spies managed to steal secret plans to the
-            Empire’s ultimate weapon, the DEATH STAR, an armored space station
-            with enough power to destroy an entire planet.
-          </p>
-          <p>
-            Pursued by the Empire’s sinister agents, Princess Leia races home
-            aboard her starship, custodian of the stolen plans that can save her
-            people and restore freedom to the galaxy…
-          </p>
-          <div className='title'>
-            <p>Episode IV</p>
-            <h3>A NEW HOPE</h3>
-            <p>May 4, 1977</p>
+          <div className="title">
+            <p>released on: {releaseDate}</p>
           </div>
         </div>
       </section>
     </div>
   );
+};
+
+Scrolling.propTypes = {
+  title: PropTypes.string,
+  episodeId: PropTypes.string,
+  openingCrawl: PropTypes.string,
+  releaseDate: PropTypes.string
 };
 
 export default Scrolling;
